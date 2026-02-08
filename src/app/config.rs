787570ab9c9e50dc76a82d445c18c7bfbd5a9136
Aleksander Hansen <aleksanderhan@gmail.com>
2026-02-08 18:38:54 +0100

    Add per-voxel normal estimation for smoother shading (#119)
    * Add per-voxel normal estimation
    
    * wip

--- src/app/config.rs
+++ src/app/config.rs
@@ -4,7 +4,7 @@
 
 pub const CHUNK_SIZE: u32 = 64;
 
-pub const ACTIVE_RADIUS: i32 = 14;
+pub const ACTIVE_RADIUS: i32 = 7;
 pub const KEEP_RADIUS: i32 = ACTIVE_RADIUS * 2;
 
 pub const VOXEL_SIZE_M_F32: f32 = 0.10;
